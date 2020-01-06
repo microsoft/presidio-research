86312fc91a5e827b6891b237851e127f5cf85cf0
Omri Mendels <omri374@users.noreply.github.com>
2020-01-07 00:10:51 +0200

    Update presidio_analyzer.py

--- presidio_evaluator/presidio_analyzer.py
+++ presidio_evaluator/presidio_analyzer.py
@@ -64,7 +64,7 @@ class PresidioAnalyzer(ModelEvaluator):
 
 if __name__ == "__main__":
     print("Reading dataset")
-    input_samples = read_synth_dataset("../data/generated_size_30000_date_July 24 2019.txt")
+    input_samples = read_synth_dataset("../data/synth_dataset.txt")
 
     print("Preparing dataset by aligning entity names to Presidio's entity names")
 
