2b4e0652b4625eae987a7b0ce4261693ead7919d
Benjamin Kilimnik <benjamin_kilimnik@brown.edu>
2022-11-01 14:02:07 -0400

    update plotting

--- presidio_evaluator/evaluation/evaluator.py
+++ presidio_evaluator/evaluation/evaluator.py
@@ -1,5 +1,6 @@
 from collections import Counter
 from typing import List, Optional, Dict
+from enum import Enum
 
 import numpy as np
 from tqdm import tqdm
@@ -344,7 +345,7 @@ class Evaluator:
             ((beta ** 2) * precision) + recall
         )
 
-    class ErrorAnalyzer:
+    class Plotter:
         def __init__(self, model, results, errors, output_folder, model_name):
             self.model = model
             self.results = results
@@ -352,29 +353,35 @@ class Evaluator:
             self.output_folder = output_folder
             self.model_name = model_name.replace("/", "-")
 
-        def plot_recall_precision_f2(self) -> None:
-            """Plot per-entity recall and precision"""
-            d = {}
-            d['entity'] = deepcopy(list(self.results.entity_recall_dict.keys()))
-            d['recall'] = deepcopy(list(self.results.entity_recall_dict.values()))
-            d['precision'] = deepcopy(
-                list(self.results.entity_precision_dict.values()))
-            d['count'] = deepcopy(list(self.results.n_dict.values()))
-            d['f2_score'] = [Evaluator.f_beta(precision=precision, recall=recall, beta=2.5)
-                             for recall, precision in zip(d['recall'], d['precision'])]
-            df = pd.DataFrame(d)
-            df['model'] = self.model_name
-            # self._plot(df, plot_type="Recall")
-            # self._plot(df, plot_type="Precision")
-            self._plot(df, plot_type="F2_Score")
+        class PlotType(Enum):
+            PRECISION = "precision"
+            RECALL = "recall"
+            F2_SCORE = "f2_score"
 
+        def plot(self, plot_type=self.PlotType.F2_SCORE) -> None:
+            """
+            Plots per-entity recall, precision, or F2 score for evaluated model. 
+            :param plot_type: which metric to graph (default is F2 score)
+            """
+            scores = {}
+            scores['entity'] = deepcopy(list(self.results.entity_recall_dict.keys()))
+            scores['recall'] = deepcopy(list(self.results.entity_recall_dict.values()))
+            scores['precision'] = deepcopy(
+                list(self.results.entity_precision_dict.values()))
+            scores['count'] = deepcopy(list(self.results.n_dict.values()))
+            scores['f2_score'] = [Evaluator.f_beta(precision=precision, recall=recall, beta=2.5)
+                                  for recall, precision in zip(scores['recall'], scores['precision'])]
+            df = pd.DataFrame(scores)
+            df['model'] = self.model_name
+            self._plot(df, plot_type=plot_type)
+            # save scores dict to disk, useful when plotting multiple models
             scores_output = self.output_folder / \
                 f"scores-dict-{self.model_name}.json"
             df.to_csv(scores_output, index=False)
 
-        def _plot(self, df, plot_type: str) -> None:
+        def _plot(self, df, plot_type) -> None:
             fig = px.bar(df, text_auto=".2", y='entity', orientation="h",
-                         x=f'{plot_type.lower()}', color='count', barmode='group', title=f"Per-entity {plot_type} for {self.model_name}")
+                         x=plot_type, color='count', barmode='group', title=f"Per-entity {plot_type} for {self.model_name}")
             fig.update_layout(barmode='group', yaxis={
                 'categoryorder': 'total ascending'})
             fig.update_layout(yaxis_title=f"{plot_type}", xaxis_title="PII Entity")
@@ -393,44 +400,12 @@ class Evaluator:
                     showgrid=False  # Removes X-axis grid lines
                 ),
             )
-            filename = self.output_folder / \
-                f"{plot_type.lower()}_{self.model_name}.html"
-            plotly.offline.plot(
-                fig, filename=str(filename))
-            filename = self.output_folder / \
-                f"{plot_type.lower()}_{self.model_name}.png"
+            fig.show()
+            filename = self.output_folder / f"{plot_type}_{self.model_name}.png"
             fig.write_image(filename)
 
-        def _plot_multiple_models(score_files: List[Path], model_name: str, plot_type: str) -> None:
-            """Plot per-entity Recall, Precision or F2 Score for multiple models given dataframes saved to csv during evaluation"""
-            df = pd.read_csv(score_files[0])
-            # combine score dataframes
-            for file in score_files[1:]:
-                df = pd.concat([df, pd.read_csv(file)])
-
-            fig = px.bar(df, text_auto=".2", x='entity',
-                         y=f'{plot_type.lower()}', color='model', barmode='group', title=f"Per-entity {plot_type} for {model_name}")
-            fig.update_layout(barmode='group', xaxis={
-                'categoryorder': 'total ascending'})
-            fig.update_layout(yaxis_title=f"{plot_type}", xaxis_title="PII Entity")
-            fig.update_traces(textfont_size=12, textangle=0,
-                              textposition="outside", cliponaxis=False)
-            fig.update_layout(
-                plot_bgcolor="#FFF",
-                xaxis=dict(
-                    title="PII entity",
-                    linecolor="#BCCCDC",  # Sets color of X-axis line
-                    showgrid=False  # Removes X-axis grid lines
-                ),
-                yaxis=dict(
-                    title="Recall",
-                    linecolor="#BCCCDC",  # Sets color of X-axis line
-                    showgrid=False  # Removes X-axis grid lines
-                ),
-            )
-            fig.show()
-
-        def save_errors(self):
+        def save_errors(self) -> None:
+            """Save false positive and false negative errors dataframes to csv."""
             ModelError.most_common_fp_tokens(self.errors)
 
             for entity in self.model.entity_mapping.values():
@@ -438,16 +413,27 @@ class Evaluator:
                 if fps_df is not None:
                     fps_df.to_csv(self.output_folder /
                                   f"{self.model_name}-{entity}-fps.csv")
-                    fps_df = pd.read_csv(self.output_folder /
-                                         f"{self.model_name}-{entity}-fps.csv")
                 fns_df = ModelError.get_fns_dataframe(self.errors, entity=[entity])
                 if fns_df is not None:
                     fns_df.to_csv(self.output_folder /
                                   f"{self.model_name}-{entity}-fns.csv")
-                    fns_df = pd.read_csv(self.output_folder /
-                                         f"{self.model_name}-{entity}-fns.csv")
 
-        def graph_most_common_tokens(self):
+        def graph_most_common_tokens(self) -> None:
+            """Graph most common false positive and false negative tokens for each entity."""
+
+            ModelError.most_common_fp_tokens(self.errors)
+            fps_frames = []
+            fns_frames = []
+
+            for entity in self.model.entity_mapping.values():
+                fps_df = ModelError.get_fps_dataframe(self.errors, entity=[entity])
+                if fps_df is not None:
+                    # fps_df.to_csv(self.output_folder /
+                    #               f"{self.model_name}-{entity}-fps.csv")
+                fns_df = ModelError.get_fns_dataframe(self.errors, entity=[entity])
+                if fns_df is not None:
+                    # fns_df.to_csv(self.output_folder /
+                    #               f"{self.model_name}-{entity}-fns.csv")
 
             def group(df):
                 return df.groupby(['token', 'annotation']).size().to_frame(
