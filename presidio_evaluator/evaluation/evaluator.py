25927d47e8480f67819a9a107f19168fd6a535c1
Benjamin Kilimnik <benjamin_kilimnik@brown.edu>
2022-11-02 13:48:40 -0400

    update formatting

--- presidio_evaluator/evaluation/evaluator.py
+++ presidio_evaluator/evaluation/evaluator.py
@@ -38,7 +38,7 @@ class Evaluator:
             self.entities_to_keep = self.model.entities
 
     def compare(self, input_sample: InputSample, prediction: List[str]):
-        
+
         """
         Compares ground truth tags (annotation) and predicted (prediction)
         :param input_sample: input sample containing list of tags with scheme
