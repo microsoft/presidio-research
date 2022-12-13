4bacaada40bcfb4b729831a65c1f371ab415c32f
Trang Nguyen <85181462+tranguyen221@users.noreply.github.com>
2022-12-13 11:01:38 +0100

    Update __init__.py

--- presidio_evaluator/evaluation/__init__.py
+++ presidio_evaluator/evaluation/__init__.py
@@ -1,5 +1,6 @@
 from .model_error import ModelError
 from .evaluation_result import EvaluationResult
 from .evaluator import Evaluator
+from .span_evaluator import SpanEvaluator
 
-__all__ = ["ModelError", "EvaluationResult", "Evaluator"]
+__all__ = ["ModelError", "EvaluationResult", "Evaluator", "SpanEvaluator"]
