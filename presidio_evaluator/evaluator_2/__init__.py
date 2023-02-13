45c42f2457289c815790b16e22225894a25f8457
tranguyen221 <85181462+tranguyen221@users.noreply.github.com>
2023-02-13 09:14:12 +0100

    update init

--- presidio_evaluator/evaluator_2/__init__.py
+++ presidio_evaluator/evaluator_2/__init__.py
@@ -2,5 +2,15 @@ from .evaluator_objects import SpanOutput, TokenOutput, ModelPrediction
 from .sample_error import SampleError
 from .evaluation_result import EvaluationResult
 from .evaluator import Evaluator
+from . import evaluation_helpers
 
-__all__ = ["SpanOutput", "TokenOutput", "ModelPrediction", "SampleError", "EvaluationResult"]
+
+__all__ = [
+    "SpanOutput",
+    "TokenOutput",
+    "ModelPrediction",
+    "SampleError",
+    "EvaluationResult",
+    "Evaluator",
+    "evaluation_helpers"
+]
