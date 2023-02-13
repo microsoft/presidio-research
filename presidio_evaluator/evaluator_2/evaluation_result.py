4a7874ea65cf1ae83841a6358a07b395409f2513
tranguyen221 <85181462+tranguyen221@users.noreply.github.com>
2023-02-13 11:41:43 +0100

    imp get_span_eval_schema

--- presidio_evaluator/evaluator_2/evaluation_result.py
+++ presidio_evaluator/evaluator_2/evaluation_result.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from copy import deepcopy
 from typing import List, Dict, Tuple
 
 from presidio_evaluator.evaluator_2 import SampleError
@@ -26,9 +27,7 @@ class EvaluationResult:
             self,
             sample_errors: List[SampleError],
             token_confusion_matrix: Counter = None,
-            token_model_metrics: Dict[str, Counter] = None,
-            span_model_metrics: Dict[str, Counter] = None
-
+            token_model_metrics: Dict[str, Counter] = None
     ):
         """
         Constructs all the necessary attributes for the EvaluationResult object
@@ -42,7 +41,18 @@ class EvaluationResult:
         self.sample_errors = sample_errors
         self.token_confusion_matrix = token_confusion_matrix
         self.token_model_metrics = token_model_metrics
-        self.span_model_metrics = span_model_metrics
+        # Initialize span metrics
+        # set up a dict for storing the span metrics
+        self.span_category_output = {'correct': 0, 'incorrect': 0, 'partial': 0, 'missed': 0, 'spurious': 0}
+        # copy results dict to cover the four evaluation schemes for PII.
+        self.span_pii_eval = {'strict': Counter(self.span_category_output),
+                              'ent_type': Counter(self.span_category_output),
+                              'partial': Counter(self.span_category_output),
+                              'exact': Counter(self.span_category_output)}
+        # copy results dict to cover the four evaluation schemes for each entity in entities_to_keep.
+        self.span_model_metrics = {e: deepcopy(self.span_pii_eval) for e in self.entities_to_keep}
+        self.span_model_metrics['overall'] = deepcopy(self.span_pii_eval)
+
 
     def to_log(self) -> Dict:
         """
@@ -55,3 +65,40 @@ class EvaluationResult:
         Convert the EvaluationResult to display confusion matrix to the end user
         """
         pass
+
+    def get_span_eval_schema(self) -> Dict[str, Dict[str, Counter]]:
+        """Update the evaluation schema with the new schema.
+
+        param:span_outputs (dict): The new schema to update the evaluation schema with.
+        returns: dict: The updated evaluation schema.
+        """
+        for sample_error in self.sample_errors:
+            span_outputs = sample_error.span_outputs
+            for span_output in span_outputs:
+                entity_list = ['overall', span_output.annotated_span.entity_type]
+                if span_output.output_type == "STRICT":
+                    eval_list = ["strict", "ent_type", "partial", "exact"]
+                    for eval_type, entity in zip(eval_list, entity_list):
+                        self.span_model_metrics[entity][eval_type]["correct"] += 1
+                elif span_output.output_type == "EXACT":
+                    for eval_type, entity in zip(["strict", "ent_type"], entity_list):
+                        self.span_model_metrics[entity][eval_type]["incorrect"] += 1
+                    for eval_type, entity in zip(["partial", "exact"], entity_list):
+                        self.span_model_metrics[entity][eval_type]["correct"] += 1
+                elif span_output.output_type == "ENT_TYPE":
+                    for entity in entity_list:
+                        self.span_model_metrics[entity]["strict"]["incorrect"] += 1
+                        self.span_model_metrics[entity]["ent_type"]["correct"] += 1
+                        self.span_model_metrics[entity]["partial"]["partial"] += 1
+                        self.span_model_metrics[entity]["exact"]["incorrect"] += 1
+                elif span_output.output_type == "PARTIAL":
+                    for eval_type, entity in zip(["strict", "ent_type", "exact"], entity_list):
+                        self.span_model_metrics[entity][eval_type]["incorrect"] += 1
+                    self.span_model_metrics["overall"]["partial"]["partial"] += 1
+                    self.span_model_metrics[span_output.annotated_span.entity_type]["partial"]["partial"] += 1
+                elif span_output.output_type == "SPURIOUS":
+                    for eval_type, entity in zip(["strict", "ent_type", "partial", "exact"], entity_list):
+                        self.span_model_metrics[entity][eval_type]["spurious"] += 1
+                elif span_output.output_type == "MISSED":
+                    for eval_type, entity in zip(["strict", "ent_type", "partial", "exact"], entity_list):
+                        self.span_model_metrics[entity][eval_type]["missed"] += 1
