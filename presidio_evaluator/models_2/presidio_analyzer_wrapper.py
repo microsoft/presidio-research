31fad0575bfd4809a7a50cd46e62b7fa03e8fd9c
tranguyen221 <85181462+tranguyen221@users.noreply.github.com>
2023-02-17 10:44:34 +0100

    refactor PresidioAnalyzerWrapper

--- presidio_evaluator/models_2/presidio_analyzer_wrapper.py
+++ presidio_evaluator/models_2/presidio_analyzer_wrapper.py
@@ -2,20 +2,21 @@ from typing import List, Optional, Dict
 
 from presidio_analyzer import AnalyzerEngine
 
-from presidio_evaluator import InputSample, span_to_tag
-from presidio_evaluator.models import BaseModel
+from presidio_evaluator import InputSample, Span
+from presidio_evaluator.models_2 import BaseModel
+from presidio_evaluator.evaluator_2 import ModelPrediction
 
 
 class PresidioAnalyzerWrapper(BaseModel):
     def __init__(
-        self,
-        analyzer_engine: Optional[AnalyzerEngine] = None,
-        entities_to_keep: List[str] = None,
-        verbose: bool = False,
-        labeling_scheme: str = "BIO",
-        score_threshold: float = 0.4,
-        language: str = "en",
-        entity_mapping: Optional[Dict[str, str]] = None,
+            self,
+            analyzer_engine: Optional[AnalyzerEngine] = None,
+            entities_to_keep: List[str] = None,
+            verbose: bool = False,
+            labeling_scheme: str = "BIO",
+            score_threshold: float = 0.4,
+            language: str = "en",
+            entity_mapping: Optional[Dict[str, str]] = None,
     ):
         """
         Evaluation wrapper for the Presidio Analyzer
@@ -30,44 +31,46 @@ class PresidioAnalyzerWrapper(BaseModel):
         self.score_threshold = score_threshold
 
         if analyzer_engine:
-            assert language in analyzer_engine.supported_languages,\
-                f'Provided analyzer supports {analyzer_engine.supported_languages} which does not include {language}'
+            assert language in analyzer_engine.supported_languages, \
+                f'Provided analyzer supports {analyzer_engine.supported_languages} ' \
+                f'which does not include {language}'
         else:
             analyzer_engine = AnalyzerEngine(supported_languages=[language])
         self.language = language
         self._update_recognizers_based_on_entities_to_keep(analyzer_engine)
         self.analyzer_engine = analyzer_engine
 
-    def predict(self, sample: InputSample) -> List[str]:
+    def predict(self, sample: InputSample) -> ModelPrediction:
+        """
+        Returns the predicted tokens/spans from the evaluated model
+        :param sample: Sample to be evaluated
+        :return: ModelPrediction object
+        """
         results = self.analyzer_engine.analyze(
             text=sample.full_text,
             entities=self.entities,
             language=self.language,
             score_threshold=self.score_threshold,
         )
-        starts = []
-        ends = []
-        scores = []
-        tags = []
+        # Convert Presidio results to the List of Span format
+        response_spans = []
 
         for res in results:
-            starts.append(res.start)
-            ends.append(res.end)
-            tags.append(res.entity_type)
-            scores.append(res.score)
+            # Create output for span-level evaluation
+            predict_span = Span(entity_type=res.entity_type,
+                                start_position=res.start,
+                                end_position=res.end,
+                                entity_value=sample.full_text[res.start:res.end],
+                                score=res.score)
+            response_spans.append(predict_span)
 
-        response_tags = span_to_tag(
-            scheme="IO",
-            text=sample.full_text,
-            starts=starts,
-            ends=ends,
-            tokens=sample.tokens,
-            scores=scores,
-            tags=tags,
+        return ModelPrediction(
+            input_sample=sample,
+            predicted_spans=response_spans
         )
-        return response_tags
 
-    # Mapping between dataset entities and Presidio entities. Key: Dataset entity, Value: Presidio entity
+    # Mapping between dataset entities and Presidio entities.
+    # Key: Dataset entity, Value: Presidio entity
     presidio_entities_map = dict(PERSON="PERSON",
                                  GPE="LOCATION",
                                  EMAIL_ADDRESS="EMAIL_ADDRESS",
@@ -96,12 +99,13 @@ class PresidioAnalyzerWrapper(BaseModel):
                                  O="O")
 
     def _update_recognizers_based_on_entities_to_keep(
-        self, analyzer_engine: AnalyzerEngine
+            self, analyzer_engine: AnalyzerEngine
     ):
         """Check if there are any entities not supported by this presidio instance.
         Add ORGANIZATION as it is removed by default
         """
-        supported_entities = analyzer_engine.get_supported_entities(language=self.language)
+        supported_entities = analyzer_engine.get_supported_entities(
+            language=self.language)
         print("Entities supported by this Presidio Analyzer instance:")
         print(", ".join(supported_entities))
 
@@ -109,12 +113,16 @@ class PresidioAnalyzerWrapper(BaseModel):
             self.entities = supported_entities
         for entity in self.entities:
             if entity not in supported_entities:
-                print(f"Entity {entity} is not supported by this instance of Presidio Analyzer Engine")
+                print(
+                    f"Entity {entity} is not supported by "
+                    f"this instance of Presidio Analyzer Engine")
 
         if "ORGANIZATION" in self.entities and "ORGANIZATION" not in supported_entities:
             recognizers = analyzer_engine.get_recognizers()
             spacy_recognizer = [rec for rec in recognizers
-                                if rec.name == "SpacyRecognizer" or rec.name == "StanzaRecognizer"]
+                                if
+                                rec.name == "SpacyRecognizer"
+                                or rec.name == "StanzaRecognizer"]
             if len(spacy_recognizer):
                 spacy_recognizer = spacy_recognizer[0]
                 spacy_recognizer.supported_entities.append("ORGANIZATION")
