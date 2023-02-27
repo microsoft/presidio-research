3df9bf72e13e7a8194efcb28ce00433190c9c746
tranguyen221 <85181462+tranguyen221@users.noreply.github.com>
2023-02-27 10:44:38 +0100

    Update SampleError class

--- presidio_evaluator/evaluator_2/sample_error.py
+++ presidio_evaluator/evaluator_2/sample_error.py
@@ -6,30 +6,13 @@ from presidio_evaluator.evaluator_2 import TokenOutput, SpanOutput
 
 @dataclass
 class SampleError:
-    """
-    Holds information about token and span errors for made a given sample for
-    analysis purposes
-    ...
-
-    Attributes
-    ----------
-    full_text : str
-        the full input text from InputSample
-    metadata : Dict
-        the metadata on text from InputSample
-    token_output : List[TokenOutput]
-        list of token errors of a given model for a sample
-    span_output: List[SpanOutput]
-        list of span outputs of a given model for a sample
-    -------
-    """
-
     def __init__(
         self,
         full_text: str,
         metadata: Dict = None,
         token_output: List[TokenOutput] = None,
         span_output: List[SpanOutput] = None,
+        entities_to_keep: List[str] = None,
     ):
         """
         Constructs all the necessary attributes for the SampleError object
@@ -37,11 +20,13 @@ class SampleError:
         :param metadata: metadata on text from InputSample
         :param token_output: list of token errors of a given model for a sample
         :param span_output: list of span outputs of a given model for a sample
+        :param entities_to_keep: list of entities to keep
         """
         self.full_text = full_text
         self.metadata = metadata
         self.token_output = token_output
         self.span_output = span_output
+        self.entities_to_keep = entities_to_keep
 
     def __str__(self):
         """Return str(self)."""
@@ -49,8 +34,10 @@ class SampleError:
             "Full text = {}, "
             "Token errors = {}, "
             "Span outputs = {}, "
+            "Entities to keep = {}, "
             "Metadata = {}".format(
-                self.full_text, self.token_output, self.span_output, self.metadata
+                self.full_text, self.token_output, self.span_output,
+                self.entities_to_keep, self.metadata
             )
         )
 
