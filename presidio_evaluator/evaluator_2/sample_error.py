f05c5e772d7ded02abf92dacfd14875441795d0e
tranguyen221 <85181462+tranguyen221@users.noreply.github.com>
2023-02-28 16:05:15 +0100

    Update SampleError class

--- presidio_evaluator/evaluator_2/sample_error.py
+++ presidio_evaluator/evaluator_2/sample_error.py
@@ -8,11 +8,10 @@ from presidio_evaluator.evaluator_2 import TokenOutput, SpanOutput
 class SampleError:
     def __init__(
         self,
-        full_text: str,
+        full_text: str = None,
         metadata: Dict = None,
         token_output: List[TokenOutput] = None,
-        span_output: List[SpanOutput] = None,
-        entities_to_keep: List[str] = None,
+        span_output: List[SpanOutput] = None
     ):
         """
         Constructs all the necessary attributes for the SampleError object
@@ -20,15 +19,13 @@ class SampleError:
         :param metadata: metadata on text from InputSample
         :param token_output: list of token errors of a given model for a sample
         :param span_output: list of span outputs of a given model for a sample
-        :param entities_to_keep: list of entities to keep
         """
         self.full_text = full_text
         self.metadata = metadata
         self.token_output = token_output
         self.span_output = span_output
-        self.entities_to_keep = entities_to_keep
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return str(self)."""
         return (
             "Full text = {}, "
@@ -41,5 +38,6 @@ class SampleError:
             )
         )
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """ Return repr(self). """
         return f"<ModelError {self.__str__()}"
