2fc66edf64d62334a8608b047d1ea56490082266
tranguyen221 <85181462+tranguyen221@users.noreply.github.com>
2023-02-28 16:07:38 +0100

    Update cal_span_metrics function

--- presidio_evaluator/evaluator_2/evaluation_result.py
+++ presidio_evaluator/evaluator_2/evaluation_result.py
@@ -8,9 +8,9 @@ from presidio_evaluator.evaluator_2 import (SampleError,
 
 class EvaluationResult:
     def __init__(
-        self,
-        sample_errors: List[SampleError] = None,
-        entities_to_keep: List[str] = None,
+            self,
+            sample_errors: List[SampleError] = None,
+            entities_to_keep: List[str] = None,
     ):
         """
         Constructs all the necessary attributes for the EvaluationResult object
@@ -62,8 +62,9 @@ class EvaluationResult:
             span_outputs += sample_error.span_output
         entities_to_keep = self.entities_to_keep
 
-        span_eval_schema = span_evaluation_helpers.\
+        span_eval_schema = span_evaluation_helpers. \
             get_span_eval_schema(span_outputs, entities_to_keep)
+
         # Step 2: Calculate the precision and recall for each entity type
         span_model_metrics = {}
         for key, value in span_eval_schema.items():
@@ -72,18 +73,15 @@ class EvaluationResult:
                     span_eval_schema[key]
                 )
 
-        # Step 3: Convert span_output to a pandas DataFrame
-        entities_df = span_evaluation_helpers.span_output_to_df(span_outputs)
-
-        # Step 4: Calculate the f1 and fb score
+        # Step 3: Calculate the f1 and fb score
         for entity, value in span_model_metrics.items():
             for k, v in value.items():
                 span_model_metrics[entity][k]['f1_score'] = span_evaluation_helpers. \
                     span_f1_score(v['precision'], v['recall'])
-                span_model_metrics[entity][k]['fb_score'] = span_evaluation_helpers.\
+                span_model_metrics[entity][k]['fb_score'] = span_evaluation_helpers. \
                     span_fb_score(v['precision'], v['recall'])
 
-        # Step 5: Convert result to dataframe for display purpose
+        # Step 4: Convert result to dataframe for display purpose
         span_eval_df = self.to_span_df(span_eval_schema)
         span_metric_df = self.to_span_df(span_model_metrics)
         span_results = span_eval_df.merge(span_metric_df, on=['entity', 'eval_type'])
