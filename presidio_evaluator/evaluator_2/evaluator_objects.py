7afb3a3d26576441369f40bfab0acf9a9f1e1430
tranguyen221 <85181462+tranguyen221@users.noreply.github.com>
2023-03-03 10:22:11 +0100

    implement tag_to_span function

--- presidio_evaluator/evaluator_2/evaluator_objects.py
+++ presidio_evaluator/evaluator_2/evaluator_objects.py
@@ -1,18 +1,19 @@
 from typing import Optional, List
 import math
 
-from spacy.tokens import Token
+import spacy
+from spacy.tokens import Token, Doc
 
 from presidio_evaluator import Span, InputSample
 
 
 class TokenOutput:
     def __init__(
-        self,
-        error_type: str,
-        annotated_tag: str,
-        predicted_tag: str,
-        token: Token,
+            self,
+            error_type: str,
+            annotated_tag: str,
+            predicted_tag: str,
+            token: Token,
     ):
         """
         Constructs all the necessary attributes for the TokenOutput object
@@ -44,10 +45,10 @@ class TokenOutput:
 
     @staticmethod
     def get_token_error_by_type(
-        errors=List["TokenOutput"],
-        error_type=str,
-        entity: List[str] = None,
-        n: Optional[int] = None,
+            errors=List["TokenOutput"],
+            error_type=str,
+            entity: List[str] = None,
+            n: Optional[int] = None,
     ) -> List["TokenOutput"]:
         """
         Print the n most common tokens by error type
@@ -64,11 +65,11 @@ class TokenOutput:
 
 class SpanOutput:
     def __init__(
-        self,
-        output_type: str,
-        overlap_score: float,
-        annotated_span: Span = None,
-        predicted_span: Span = None,
+            self,
+            output_type: str,
+            overlap_score: float,
+            annotated_span: Span = None,
+            predicted_span: Span = None,
     ):
         """
         Constructs all the necessary attributes for the SpanOutput object
@@ -96,15 +97,15 @@ class SpanOutput:
     def __eq__(self, other) -> bool:
         """Compare two SpanOutput objects."""
         return (
-            self.output_type == other.output_type
-            and math.isclose(self.overlap_score, other.overlap_score)
-            and self.annotated_span == other.annotated_span
-            and self.predicted_span == other.predicted_span
+                self.output_type == other.output_type
+                and math.isclose(self.overlap_score, other.overlap_score)
+                and self.annotated_span == other.annotated_span
+                and self.predicted_span == other.predicted_span
         )
 
     @staticmethod
     def get_span_output_by_type(
-        outputs=List["SpanOutput"], output_type=str, entity: List[str] = None
+            outputs=List["SpanOutput"], output_type=str, entity: List[str] = None
     ) -> List["SpanOutput"]:
         """
         Get the list of span output by output type
@@ -119,10 +120,10 @@ class SpanOutput:
 
 class ModelPrediction:
     def __init__(
-        self,
-        input_sample: InputSample,
-        predicted_tags: List[str] = None,
-        predicted_spans: List[Span] = None
+            self,
+            input_sample: InputSample,
+            predicted_tags: List[str] = None,
+            predicted_spans: List[Span] = None
     ):
         """
         Constructs all the necessary attributes for the ModelPrediction object
@@ -143,8 +144,98 @@ class ModelPrediction:
         pass
 
     @staticmethod
-    def tag_to_span(predicted_tags: List[str]) -> List[Span]:
+    def tokenize(text, model_version="en_core_web_sm") -> Doc:
+        """
+        Tokenizes a text using a spacy model
+        :param text: str, text to tokenize
+        :param model_version: str, spacy model version
+        :return: spacy Doc object
+        """
+        nlp = spacy.load(model_version)
+        return nlp(text)
+
+    @staticmethod
+    def io_to_bio(tags):
+        """
+        Translates IO - only In or Out of entity to BIO.
+        ['PERSON','PERSON','PERSON'] is translated to
+        ['B-PERSON','I-PERSON','I-PERSON'] is translated into
+        :param tags: the input tags in IO/BILUO/BIO format
+        :return: a new list of BIO tags if the input is IO,
+        otherwise the input is returned
+        """
+        new_tags = []
+        # check if the tags are in IO format
+        if all("-" not in tag or tag[0] == 'O' for tag in tags):
+            for i, tag in enumerate(tags):
+                if tag == 'O':
+                    new_tags.append(tag)
+                elif i == 0 or tags[i - 1] != 'O':
+                    new_tags.append('B-' + tag)
+                elif tag == tags[i - 1]:
+                    new_tags.append('I-' + tag)
+                else:
+                    new_tags.append('B-' + tag)
+            return new_tags
+        else:
+            return tags
+
+    def tag_to_span(self, predicted_tags: List[str], full_text) -> List[Span]:
         """
         Turns a list of tokens with corresponding labels, into a list of span
+        :param predicted_tags: List[str], list of predicted tags
+        :param full_text: str, full text of the input sample
+        :return: List[Span], list of predicted spans
         """
-        pass
+        named_entities = []
+        start_position = None
+        end_position = None
+        ent_type = None
+        tokens = self.tokenize(full_text)
+        # If IO, translate to BIO for easier manipulation
+        predicted_tags = self.io_to_bio(predicted_tags)
+        print(predicted_tags)
+
+        for i, tag in enumerate(predicted_tags):
+            if tag == "O":
+                if ent_type is not None and start_position is not None:
+                    print(tokens[i].idx, tokens[i].text)
+                    end_position = tokens[i - 1].idx + len(tokens[i - 1].text)
+                    named_entities.append(
+                        Span(
+                            start_position=start_position,
+                            end_position=end_position,
+                            entity_type=ent_type,
+                            entity_value=full_text[start_position:end_position]
+                        )
+                    )
+                    start_position = None
+                    end_position = None
+                    ent_type = None
+            # Tag is not O
+            # This entity is not kept track yet
+            elif ent_type is None:
+                ent_type = tag[2:]
+                start_position = tokens[i].idx
+            # This is the new entity type
+            elif ent_type != tag[2:] or (ent_type == tag[2:] and tag[0] == "B"):
+                end_position = tokens[i - 1].idx + len(tokens[i - 1].text)
+                named_entities.append(Span(
+                    start_position=start_position,
+                    end_position=end_position,
+                    entity_type=ent_type,
+                    entity_value=full_text[start_position:end_position]))
+                # start a new entity
+                start_position = tokens[i].idx
+                ent_type = tag[2:]
+                end_position = None
+
+        # catches an entity that goes to the last token
+        if ent_type is not None and start_position is not None and end_position is None:
+            named_entities.append(Span(
+                start_position=start_position,
+                end_position=len(full_text),
+                entity_type=ent_type,
+                entity_value=full_text[start_position:end_position]))
+
+        return named_entities
